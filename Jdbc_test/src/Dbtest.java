1eb318eb4142759deb5eaa8d78cc0e1e854116ad
Jonathan Lam <jlam323@uwo.ca>
2017-06-01 19:33:57 -0400

    Created functions to add new user to the database and to see if a user already exists
    Also moved connect/disconnect database functions around for good coding
    practices.

--- Jdbc_test/src/Dbtest.java
+++ Jdbc_test/src/Dbtest.java
@@ -1,33 +1,172 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
 
 public class Dbtest {
 
-    private static void connect() {
+
+    public static void main (String[] args) throws IOException {
+        // Create connection object
+        Connection conn;
+        conn = connect();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /* TEST TO ADD USER*/
+        int userID;
+        System.out.println("Enter user ID");
+        userID = Integer.parseInt(br.readLine());
+
+        addUser(conn, userID, "JLam", "M");
+
+        System.out.println("Enter a user ID to be searched");
+        userID = Integer.parseInt(br.readLine());
+
+        findUser(conn, userID);
+
+        // Close the connection
+        disconnect(conn);
+    }
+
+
+    /**
+     *  Establishes a connection to the local SQLite database
+     *
+     *  @return connection object to the database
+     */
+    private static Connection connect() {
+
+        // Create connection object
         Connection conn = null;
+
         try {
+
+            // Database location and connection string
             String dbLocation = "sqlite/db/TRYBE.DB";
             String url = "jdbc:sqlite:" + dbLocation;
+
             conn = DriverManager.getConnection(url);
 
-            System.out.println("Connection to SQLITE db is successful");
+            // Connection confirmation
+            System.out.println("Connection to SQLITE db is successful.");
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
+        }
+        return conn;
+    }
+
+
+    /**
+     *  Receives the connection object and closes the connection
+     *
+     *  @param conn connection object
+     */
+    private static void disconnect(Connection conn){
+        try {
+            if (conn != null) {
+                conn.close();
+                System.out.println("Connection successfully closed.");
             }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
         }
     }
-    public static void main (String[] args)
-    {
-        connect();
+
+
+    /**
+     *  Find a user in the database.
+     *
+     *  @param conn connection object
+     *  @param ID ID of the new user
+     */
+    public static boolean findUser(Connection conn, int ID) {
+
+        boolean userExists = false;
+
+        // SQL statement
+        String sql = "SELECT * FROM PERSON WHERE NAMEID = ?";
+
+        // Create prepared statement
+        try (PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            // Insert values for the user
+            ps.setInt(1, ID);
+
+            // Execute the query
+            ResultSet rs = ps.executeQuery();
+
+            // Read the result of the query
+            if (rs.next()) {
+
+                // Read the NAMEID column of the query result
+                String queryResult = rs.getString("NAMEID");
+
+                // DEBUG
+                System.out.println("Query result: " + queryResult);
+
+                // Record whether or not the user is already in the database
+                if (!queryResult.equals(ID)){
+                    System.out.println("User " + ID + " exists in the database already.");
+                    userExists = true;
+                }
+            }
+            else {
+                System.out.println("User " + ID + " does not exist in the database.");
+                userExists = false;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return userExists;
+    }
+
+
+    /**
+     *  Add a new user to the database.
+     *
+     *  @param conn connection object
+     *  @param ID ID of the new user
+     *  @param name user's first and last name
+     *  @param gender user's gender (M/F/O)
+     */
+    public static int addUser(Connection conn, int ID, String name, String gender) {
+
+        // Check if the user exists before adding
+        boolean userExists;
+        userExists = findUser(conn, ID);
+
+        if (userExists) {
+            System.out.println("Found user: " + ID + " - " + name + ".");
+            System.out.println("Unable to add user.");
+            // Unsuccessful in adding user because they already exist
+            return 1;
+        }
+
+
+        // SQL statement
+        String sql = "INSERT INTO PERSON(NAMEID, NAME, POINTS, GENDER) VALUES(?,?,?,?)";
+
+        // Create prepared statement
+        try (PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            // Insert values for the user
+            ps.setInt(1, ID);
+            ps.setString(2, name);
+            ps.setInt(3, 0);
+            ps.setString(4, gender);
+
+            // Execute the prepared statement
+            ps.executeUpdate();
+
+            System.out.println("Successfully added user: " + ID + " - " + name + ".");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Successfully added the user
+        return 0;
     }
 }
 
